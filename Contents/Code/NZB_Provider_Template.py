aadc0e25cbeaf0b8666fbf58cd835cff20312013
Mike <mike.d.meier@gmail.com>
2012-10-02 19:07:03 -0400

    start laying out template for new NZB providers

---
+++ Contents/Code/NZB_Provider_Template.py
@@ -0,0 +1,10 @@
+### Use this file as a template when adding support for a new NZB Provider ###
+
+# URL used for API-based search #
+API_SEARCH_URL      =   '' 
+
+# URL used for API-based NZB downloads #
+API_DOWNLOAD_URL    =   ''
+
+def Search(user='', api_key='', query=''): ###TODO:: What other global-type parameters should be added? ###
+    return
