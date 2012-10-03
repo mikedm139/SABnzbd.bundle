1beb71f2212eaad920995cd041b4a8754a6644ff
Mike <mike.d.meier@gmail.com>
2012-10-03 10:34:44 -0400

    add more default methods to Provider template

--- Contents/Code/NZB_Provider_Template.py
+++ Contents/Code/NZB_Provider_Template.py
@@ -6,5 +6,18 @@ API_SEARCH_URL      =   ''
 # URL used for API-based NZB downloads #
 API_DOWNLOAD_URL    =   ''
 
-def Search(user='', api_key='', query=''): ###TODO:: What other global-type parameters should be added? ###
+def Search(sender, user='', api_key='', query=''): ###TODO:: What other global-type parameters should be added? ###
+    return
+
+def Add(sender, nzb_id, user='', api_key=''):
+    return
+
+def GetNZBDetails(sender, nzb_id, user='', api_key=''):
+    return
+
+################################################################################
+######## Add methods for setting provider-specific defaults below here #########
+################################################################################
+
+def SetDefaults(sender):
     return
