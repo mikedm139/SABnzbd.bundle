64cc252945894dd3424f7c97e14cff2ab53ce578
Mike <mike.d.meier@gmail.com>
2012-10-04 15:09:27 -0400

    create NZBMatrix provider profile

---
+++ Contents/Code/NZBMatrix.py
@@ -0,0 +1,44 @@
+#### Use this file as a template when adding support for a new NZB Provider ####
+##### Save modified version with filename <provider>.py, eg. NZBMatrix.py ######
+
+PROVIDER = 'NZBMatrix' 
+
+# URL used for API-based search #
+API_SEARCH_URL      =  'http://api.nzbmatrix.com/v1.1/search.php?search=%s&catid=%s&num=%s&username=%s&apikey=%s'
+                        # (query, category='', max_results='', user, api_key)
+
+# URL used for API-based NZB downloads #
+API_DOWNLOAD_URL    =   'http://api.nzbmatrix.com/v1.1/download.php?id=%s&username=%s&apikey=%s'
+                        # (nzb_id, user, api_key)
+
+def Search(sender, user='', api_key='', query=''): ###TODO:: What other global-type parameters should be added? ###
+    '''
+        should return a list of JSON objects with the following fields:
+        {
+            "title"     :   "the title of the nzb",
+            "nzb_id"    :   "the unqiue id of the nzb used by the provider",
+            "provider"  :   "same as the filename (without '.py')",
+            "summary"   :   "Build a summary string with as much detail as can
+                                be gathered just from the search response.",
+            "thumb:     :   "an image URL is available"
+        }
+    '''
+        
+    return
+
+def Add(sender, nzb_id, user='', api_key=''):
+    return
+
+def GetNZBDetails(sender, nzb_id, user='', api_key=''):
+    return
+
+################################################################################
+######## Add methods for setting provider-specific defaults below here #########
+################################################################################
+
+def SetDefaults(sender):
+    ''' return a MediaContainer with a list of DirectoryItem function callbacks
+     for setting defaults specific to this NZB Provider '''
+    dir = MediaContainer(title1=PROVIDER, title2="Set Defaults")
+    
+    return dir
